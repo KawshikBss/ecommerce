7cefd6cd5d040e4c2e8ecf098a02d0fbe61da31e
KawshikBss <kawshikbss@gmail.com>
2023-04-18 01:38:47 +0600

    added searchbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,18 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import {
     AiOutlineSearch,
     AiOutlineUser,
     AiOutlineShoppingCart,
 } from "react-icons/ai";
+import styles from '@/styles/Header.module.css'
 
 function Header() {
+    const [searchExtended, setSearchExtended] = useState(false);
+    const toggleSearchBar = () => {
+        setSearchExtended((curr) => !curr);
+    };
+
     return (
         <nav className="flex flex-row justify-between items-center p-[12px]">
             <ul className="flex flex-row justify-between items-center gap-[18px] list-none">
@@ -29,17 +35,22 @@ function Header() {
                 </h1>
             </Link>
             <ul className="flex flex-row justify-between items-center gap-[18px] list-none">
-                <li className="font-bold hover:font-black text-[22px] leading-7 tracking-[1.7px] uppercase text-[#212A2F]">
+                <li className="flex flex-row justify-between items-center font-bold text-[22px] leading-7 tracking-[1.7px] uppercase text-[#212A2F]">
+                    <input
+                        className={`w-0 border-2 border-black rounded-md mr-5 transition-all duration-300 ease-in-out ${
+                            searchExtended ? styles.search_bar_extended : "border-2 border-transparent"
+                        }`}
+                    />
                     <Link href={"/"}>
-                        <AiOutlineSearch />
+                        <AiOutlineSearch onClick={toggleSearchBar} />
                     </Link>
                 </li>
-                <li className="font-bold hover:font-black text-[22px] leading-7 tracking-[1.7px] uppercase text-[#212A2F]">
+                <li className="font-bold text-[22px] leading-7 tracking-[1.7px] uppercase text-[#212A2F]">
                     <Link href={"/user"}>
                         <AiOutlineUser />
                     </Link>
                 </li>
-                <li className="font-bold hover:font-black text-[22px] leading-7 tracking-[1.7px] uppercase text-[#212A2F]">
+                <li className="font-bold text-[22px] leading-7 tracking-[1.7px] uppercase text-[#212A2F]">
                     <Link href={"/"}>
                         <AiOutlineShoppingCart />
                     </Link>
