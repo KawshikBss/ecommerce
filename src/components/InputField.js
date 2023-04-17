77a87baa1e6ad6ae67965a65b0783a23e529f67a
KawshikBss <kawshikbss@gmail.com>
2023-04-18 00:48:28 +0600

    created custom inout field component

---
+++ src/components/InputField.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function InputField({ label, handleChange, secureInput = false }) {
+    return (
+        <div className="w-full flex flex-col justify-between items-start gap-[8px]">
+            <span className="font-semibold text-[0.75rem] uppercase tracking-[1px]">
+                {label ? label : "Label"}
+            </span>
+            <input type={secureInput? 'password': 'text'} className="w-full p-[20px] rounded-sm font-normal text-[1em]" />
+        </div>
+    );
+}
+
+export default InputField;
