6b211c13fc5e957c7c5b8c727a9c1e75030f5627
KawshikBss <kawshikbss@gmail.com>
2023-04-18 23:53:20 +0600

    added onclick handler

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function Button({ label, handleClick }) {
     return (
-        <span className="w-full font-semibold text-[0.7em] tracking-[1px] uppercase text-center py-[20px] border-2 border-black bg-black hover:bg-white text-white hover:text-black transition-all duration-300 ease-in-out cursor-pointer">
+        <span
+            onClick={handleClick}
+            className="w-full font-semibold text-[0.7em] tracking-[1px] uppercase text-center py-[20px] border-2 border-black bg-black hover:bg-white text-white hover:text-black transition-all duration-300 ease-in-out cursor-pointer"
+        >
             {label ? label : "Click Me"}
         </span>
     );
