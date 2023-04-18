f0d0b9a1744088ff1f3123b806d169841c66194d
KawshikBss <kawshikbss@gmail.com>
2023-04-18 12:48:11 +0600

    created dummy data

---
+++ src/data/products.js
@@ -0,0 +1,102 @@
+export default [
+    {
+        id: 0,
+        name: "Enim eiusmod labore",
+        price: 100,
+        discount_price: 92,
+        description:
+            "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+        image: "1.avif",
+        tags: ["New", "Trending"],
+    },
+    {
+        id: 1,
+        name: "Enim eiusmod labore",
+        price: 100,
+        discount_price: 92,
+        description:
+            "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+        image: "2.avif",
+        tags: ["New", "Trending"],
+    },
+    {
+        id: 2,
+        name: "Enim eiusmod labore",
+        price: 100,
+        discount_price: 92,
+        description:
+            "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+        image: "3.avif",
+        tags: ["New", "Trending"],
+    },
+    {
+        id: 3,
+        name: "Enim eiusmod labore",
+        price: 100,
+        discount_price: 92,
+        description:
+            "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+        image: "4.avif",
+        tags: ["New", "Trending"],
+    },
+    {
+        id: 4,
+        name: "Enim eiusmod labore",
+        price: 100,
+        discount_price: 92,
+        description:
+            "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+        image: "5.avif",
+        tags: ["New", "Trending"],
+    },
+    {
+        id: 5,
+        name: "Enim eiusmod labore",
+        price: 100,
+        discount_price: 92,
+        description:
+            "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+        image: "6.avif",
+        tags: ["New", "Trending"],
+    },
+    {
+        id: 6,
+        name: "Enim eiusmod labore",
+        price: 100,
+        discount_price: 92,
+        description:
+            "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+        image: "7.avif",
+        tags: ["New", "Trending"],
+    },
+    {
+        id: 7,
+        name: "Enim eiusmod labore",
+        price: 100,
+        discount_price: 92,
+        description:
+            "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+        image: "8.avif",
+        tags: ["New", "Trending"],
+    },
+    {
+        id: 8,
+        name: "Enim eiusmod labore",
+        price: 100,
+        discount_price: 92,
+        description:
+            "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+        image: "9.avif",
+        tags: ["New", "Trending"],
+    },
+    {
+        id: 9,
+        name: "Enim eiusmod labore",
+        price: 100,
+        discount_price: 92,
+        description:
+            "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+        image: "10.avif",
+        tags: ["New", "Trending"],
+    },
+];
