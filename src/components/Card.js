b2b9aeba00ba0b43d7d82d039cbda0cd86f3bc33
KawshikBss <kawshikbss@gmail.com>
2023-04-18 18:50:01 +0600

    added image hover animation

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,29 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function Card({ item }) {
     return (
-        <div className="min-w-[360px] h-[455px] rounded-md shadow-2xl flex flex-col justify-between">
-            <Image
-                width={360}
-                height={325}
-                src={"/" + item.image}
-                className="min-w-[360px] h-[325px] rounded-md object-cover"
-            />
-            <div className="flex flex-col p-[18px] justify-between items-start">
-                <h3 className="text-[22px] leading-[1.32] tracking-[0.5px] font-bold text-[#212A2F]">
-                    {item?.name ? item.name : "N/A"}
-                </h3>
-                <p className="text-[16px] leading-[1.5] tracking-[0.5px] font-normal text-[#212A2F]">
-                    {item?.price ? item.price : "0"}$
-                </p>
+        <Link href={`/store/${item.id}`}>
+            <div className="min-w-[330px] h-[455px] rounded-md shadow-xl hover:shadow-2xl flex flex-col justify-between transition-all duration-300 ease-in-out">
+                <div className="min-w-[330px] h-[325px] rounded-md overflow-hidden">
+                    <Image
+                        width={330}
+                        height={325}
+                        src={"/" + item.image}
+                        className="min-w-[330px] h-[325px] rounded-md object-cover hover:scale-150 transition-all duration-300 ease-in-out"
+                    />
+                </div>
+                <div className="flex flex-col p-[18px] justify-between items-start">
+                    <h3 className="text-[22px] leading-[1.32] tracking-[0.5px] font-bold text-[#212A2F]">
+                        {item?.name ? item.name : "N/A"}
+                    </h3>
+                    <p className="text-[16px] leading-[1.5] tracking-[0.5px] font-normal text-[#212A2F]">
+                        {item?.price ? item.price : "0"}$
+                    </p>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 }
 
