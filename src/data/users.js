1ba2692daf601d3b1671ae4435a7415bf33e4c95
KawshikBss <kawshikbss@gmail.com>
2023-04-18 23:55:01 +0600

    created dummy data for users

---
+++ src/data/users.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        id: 0,
+        name: "admin",
+        phone: "01700000000",
+        password: "admin",
+        admin: true,
+    },
+    {
+        id: 1,
+        name: "User 1",
+        phone: "01700000001",
+        password: "12345678",
+        admin: false,
+    },
+    {
+        id: 2,
+        name: "User 2",
+        phone: "01700000002",
+        password: "12345678",
+        admin: false,
+    },
+    {
+        id: 3,
+        name: "User 3",
+        phone: "01700000003",
+        password: "12345678",
+        admin: false,
+    },
+];
