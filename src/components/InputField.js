4956fd81b602f72ad1cb31c577ed0309c077e32c
KawshikBss <kawshikbss@gmail.com>
2023-04-18 23:53:38 +0600

    added onChange handeler

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -6,7 +6,12 @@ function InputField({ label, handleChange, secureInput = false }) {
             <span className="font-semibold text-[0.75rem] uppercase tracking-[1px]">
                 {label ? label : "Label"}
             </span>
-            <input type={secureInput? 'password': 'text'} className="w-full p-[20px] rounded-sm font-normal text-[1em]" />
+            <input
+                type={secureInput ? "password" : "text"}
+                name={label ? label : "name"}
+                className="w-full p-[20px] rounded-sm font-normal text-[1em]"
+                onChange={handleChange}
+            />
         </div>
     );
 }
