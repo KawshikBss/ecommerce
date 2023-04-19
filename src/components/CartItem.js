cbf7d103bbed4d24fbcea3b78f89db2d96eff1b0
KawshikBss <kawshikbss@gmail.com>
2023-04-19 23:13:20 +0600

    added product details link

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { AiOutlinePlus, AiOutlineMinus, AiFillDelete } from "react-icons/ai";
 
@@ -13,13 +14,17 @@ function CartItem({
     return (
         <div className="w-full flex flex-row justify-between items-start">
             <div className="w-2/3 flex flex-row justify-between items-start gap-8">
-                <Image
-                    width={200}
-                    height={200}
-                    src={"/" + item.image}
-                    alt={item.image}
-                    className="w-1/3 h-[15vh] object-cover rounded-md"
-                />
+                <div className="w-1/3 h-[15vh] rounded-md overflow-hidden">
+                    <Link href={"/store/" + item.id}>
+                        <Image
+                            width={200}
+                            height={200}
+                            src={"/" + item.image}
+                            alt={item.image}
+                            className="w-full h-[15vh] object-cover rounded-md hover:scale-150 transition-all duration-300 ease-in-out"
+                        />
+                    </Link>
+                </div>
                 <div className="w-2/3 flex flex-col justify-between items-start">
                     <h3 className="text-[14px] text-[#212A2F] font-semibold leading-6 tracking-wider">
                         {item?.name ? item.name : "Product Name"}
