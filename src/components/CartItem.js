697c50d4e30d45f2e7536e72a7b05335b8c7c8d5
KawshikBss <kawshikbss@gmail.com>
2023-04-19 15:19:15 +0600

    completed functionality

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import React from "react";
 import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 
-function CartItem({ item }) {
+function CartItem({ itemId, item, price, quantity, updateItemQuantity }) {
     return (
         <div className="w-full flex flex-row justify-between items-start">
             <div className="w-2/3 flex flex-row justify-between items-start gap-8">
@@ -28,16 +28,22 @@ function CartItem({ item }) {
             </div>
             <div className="flex flex-col justify-between items-end gap-2">
                 <h3 className="text-[14px] text-[#212A2F] font-semibold leading-6 tracking-wider">
-                    ${item?.price ? item.price : "100"}
+                    ${price ? price : "100"}
                 </h3>
                 <span className="flex flex-row justify-between items-center gap-2 border-2 border-[#212A2F] rounded-xl px-2">
-                    <span className="bg-white hover:bg-[#212A2F] text-[#212A2F] hover:text-white rounded-xl cursor-pointer  transition-all duration-300 ease-in-out">
+                    <span
+                        onClick={() => updateItemQuantity(itemId, -1)}
+                        className="bg-white hover:bg-[#212A2F] text-[#212A2F] hover:text-white rounded-xl cursor-pointer  transition-all duration-300 ease-in-out"
+                    >
                         <AiOutlineMinus />
                     </span>
                     <span className="text-[14px] text-[#212A2F] font-semibold leading-6 tracking-wider">
-                        0
+                        {quantity ? quantity : 0}
                     </span>
-                    <span className="bg-white hover:bg-[#212A2F] text-[#212A2F] hover:text-white rounded-xl cursor-pointer  transition-all duration-300 ease-in-out">
+                    <span
+                        onClick={() => updateItemQuantity(itemId, 1)}
+                        className="bg-white hover:bg-[#212A2F] text-[#212A2F] hover:text-white rounded-xl cursor-pointer  transition-all duration-300 ease-in-out"
+                    >
                         <AiOutlinePlus />
                     </span>
                 </span>
