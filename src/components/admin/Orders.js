755886e5f27d3b73d84e149583a59e09972e2b6a
KawshikBss <kawshikbss@gmail.com>
2023-04-19 18:14:13 +0600

    created orders tab

---
+++ src/components/admin/Orders.js
@@ -0,0 +1,11 @@
+import ordersdata from '@/data/ordersdata';
+import React from 'react'
+
+function Orders() {
+    console.log(ordersdata);
+  return (
+    <div>Orders</div>
+  )
+}
+
+export default Orders
