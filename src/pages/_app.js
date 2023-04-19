627021c30128182d4a32e79061039b4b49463f4c
KawshikBss <kawshikbss@gmail.com>
2023-04-19 15:20:04 +0600

    added toast notifications

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,11 +3,14 @@ import AuthContext from "@/contexts/AuthContext";
 import CartContext from "@/contexts/CartContext";
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
     return (
         <SessionProvider session={pageProps.session}>
             <CartContext>
+                <ToastContainer />
                 <DefaultLayout>
                     <Component {...pageProps} />
                 </DefaultLayout>
