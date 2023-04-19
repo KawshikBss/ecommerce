6de325a75e1df12448d0658b346350f0a0276454
KawshikBss <kawshikbss@gmail.com>
2023-04-20 00:07:57 +0600

    completed add customer component

--- src/components/admin/AddCustomer.js
+++ src/components/admin/AddCustomer.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from "react";
+import InputField from "../InputField";
+import Button from "../Button";
+import { toast } from "react-toastify";
 
 function AddCustomer() {
-  return (
-    <div>AddCustomer</div>
-  )
+    const [formData, setFormData] = useState({
+        "first name": "",
+        "last name": "",
+        phone: "",
+        password: "",
+        "confirm password": "",
+    });
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((curr) => ({
+            ...curr,
+            [name]: value,
+        }));
+    };
+    const handleSubmit = () => {
+        toast.success("Customer added successfully");
+        console.log(formData);
+    };
+    return (
+        <div className="w-full h-full bg-white flex flex-col gap-4 items-center shadow-xl p-4 rounded-md overflow-y-scroll">
+            <h3 className="text-[22px] leading-5 font-bold tracking-[0.5px] text-[#212A2F]">
+                Add new Customer
+            </h3>
+            <div className="w-1/3 bg-[#f1ebe7] p-2 rounded-md flex flex-col justify-between items-start gap-[19px]">
+                <InputField
+                    label="first name"
+                    handleChange={handleInputChange}
+                />
+                <InputField
+                    label="last name"
+                    handleChange={handleInputChange}
+                />
+                <InputField label="phone" handleChange={handleInputChange} />
+                <InputField
+                    label="password"
+                    secureInput
+                    handleChange={handleInputChange}
+                />
+                <InputField
+                    label="confirm password"
+                    secureInput
+                    handleChange={handleInputChange}
+                />
+                <Button label="Add Customer" handleClick={handleSubmit} />
+            </div>
+        </div>
+    );
 }
 
-export default AddCustomer
+export default AddCustomer;
