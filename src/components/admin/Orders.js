621ffb7b320582eb52e58459bf78b5f07c5fc430
KawshikBss <kawshikbss@gmail.com>
2023-04-19 19:11:52 +0600

    added order list and header

--- src/components/admin/Orders.js
+++ src/components/admin/Orders.js
@@ -1,11 +1,18 @@
-import ordersdata from '@/data/ordersdata';
-import React from 'react'
+import ordersdata from "@/data/ordersdata";
+import React from "react";
+import OrderListItem from "../OrderListItem";
 
 function Orders() {
-    console.log(ordersdata);
-  return (
-    <div>Orders</div>
-  )
+    return (
+        <div className="w-full h-full bg-white flex flex-col gap-4 items-start shadow-xl p-4 rounded-md overflow-y-scroll">
+            <h3 className="text-[22px] leading-5 font-bold tracking-[0.5px] text-[#212A2F]">
+                Orders List
+            </h3>
+            {ordersdata.map((order, index) => {
+                return <OrderListItem order={order} key={index} />;
+            })}
+        </div>
+    );
 }
 
-export default Orders
+export default Orders;
