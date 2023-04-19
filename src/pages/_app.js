6eb998d8ec0f74b6b9db93efabea79861cc0aa3b
KawshikBss <kawshikbss@gmail.com>
2023-04-19 14:41:52 +0600

    added cart context

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,17 @@
 import DefaultLayout from "@/components/layouts/DefaultLayout";
 import AuthContext from "@/contexts/AuthContext";
+import CartContext from "@/contexts/CartContext";
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }) {
     return (
         <SessionProvider session={pageProps.session}>
-            <DefaultLayout>
-                <Component {...pageProps} />
-            </DefaultLayout>
+            <CartContext>
+                <DefaultLayout>
+                    <Component {...pageProps} />
+                </DefaultLayout>
+            </CartContext>
         </SessionProvider>
     );
 }
