6756d0c3ca9bc2bee4155efa53e76c840f11e372
KawshikBss <kawshikbss@gmail.com>
2023-04-19 15:45:04 +0600

    protected route from unauthenticated users

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,14 +1,17 @@
 import { data } from "autoprefixer";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import React from "react";
 
 function Admin() {
     const { data: session, status } = useSession();
-    console.log({ session, status });
-    // if (status !== 'unauthenticated' || !session) return <div>Admin</div>;
+    const router = useRouter();
+    if (!session || status === "unauthenticated") {
+        router.push("/");
+    }
     const { user } = session || {};
     console.log(user);
-    return <div>User</div>;
+    return <div>Hello {user?.name}</div>;
 }
 
 export default Admin;
