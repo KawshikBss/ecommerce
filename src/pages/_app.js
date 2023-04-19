3d3e15866d65f47fb296667766351d21af3aa948
KawshikBss <kawshikbss@gmail.com>
2023-04-19 14:21:37 +0600

    removed authcontext and added session provider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,14 @@
 import DefaultLayout from "@/components/layouts/DefaultLayout";
 import AuthContext from "@/contexts/AuthContext";
 import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }) {
     return (
-        <AuthContext>
+        <SessionProvider session={pageProps.session}>
             <DefaultLayout>
                 <Component {...pageProps} />
             </DefaultLayout>
-        </AuthContext>
+        </SessionProvider>
     );
 }
