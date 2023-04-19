19026aedf0d4f54d1faafb6c027ce2310e7ed9fc
KawshikBss <kawshikbss@gmail.com>
2023-04-19 18:20:20 +0600

    created orders data to store orders

---
+++ src/data/ordersdata.js
@@ -0,0 +1,257 @@
+export default [
+    {
+        id: 1,
+        customer: "John Doe",
+        products: [
+            {
+                id: 0,
+                name: "Enim eiusmod labore",
+                description:
+                    "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+                image: "1.avif",
+                price: 10.99,
+                quantity: 2,
+            },
+            {
+                id: 1,
+                name: "Enim eiusmod labore",
+                description:
+                    "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+                image: "2.avif",
+                price: 5.99,
+                quantity: 1,
+            },
+        ],
+        total: 27.97,
+        status: "processing",
+    },
+    {
+        id: 2,
+        customer: "Jane Smith",
+        products: [
+            {
+                id: 3,
+                name: "Enim eiusmod labore",
+                description:
+                    "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+                image: "4.avif",
+                price: 7.99,
+                quantity: 3,
+            },
+            {
+                id: 0,
+                name: "Enim eiusmod labore",
+                description:
+                    "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+                image: "1.avif",
+                price: 15.99,
+                quantity: 1,
+            },
+        ],
+        total: 48.96,
+        status: "shipped",
+    },
+    {
+        id: 3,
+        customer: "Bob Johnson",
+        products: [
+            {
+                id: 1,
+                name: "Enim eiusmod labore",
+                description:
+                    "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+                image: "2.avif",
+                price: 5.99,
+                price: 20.99,
+                quantity: 2,
+            },
+        ],
+        total: 41.98,
+        status: "completed",
+    },
+    {
+        id: 4,
+        customer: "Sara Lee",
+        products: [
+            {
+                id: 0,
+                name: "Enim eiusmod labore",
+                description:
+                    "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+                image: "1.avif",
+                price: 4.99,
+                quantity: 4,
+            },
+        ],
+        total: 19.96,
+        status: "processing",
+    },
+    {
+        id: 5,
+        customer: "Mike Johnson",
+        products: [
+            {
+                id: 1,
+                name: "Enim eiusmod labore",
+                description:
+                    "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+                image: "2.avif",
+                price: 5.99,
+                price: 8.99,
+                quantity: 1,
+            },
+            {
+                id: 2,
+                name: "Enim eiusmod labore",
+                description:
+                    "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+                image: "3.avif",
+                price: 12.99,
+                quantity: 2,
+            },
+        ],
+        total: 34.97,
+        status: "shipped",
+    },
+    {
+        id: 6,
+        customer: "Emily Chen",
+        products: [
+            {
+                id: 3,
+                name: "Enim eiusmod labore",
+                description:
+                    "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+                image: "4.avif",
+                price: 16.99,
+                quantity: 1,
+            },
+        ],
+        total: 16.99,
+        status: "completed",
+    },
+    {
+        id: 7,
+        customer: "Jackie Smith",
+        products: [
+            {
+                id: 2,
+                name: "Enim eiusmod labore",
+                description:
+                    "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+                image: "3.avif",
+                price: 9.99,
+                quantity: 3,
+            },
+            {
+                id: 0,
+                name: "Enim eiusmod labore",
+                description:
+                    "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+                image: "1.avif",
+                price: 11.99,
+                quantity: 2,
+            },
+        ],
+        total: 65.95,
+        status: "processing",
+    },
+    {
+        id: 8,
+        customer: "David Brown",
+        products: [
+            {
+                id: 3,
+                name: "Enim eiusmod labore",
+                description:
+                    "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+                image: "4.avif",
+                price: 22.99,
+                quantity: 1,
+            },
+            {
+                id: 1,
+                name: "Enim eiusmod labore",
+                description:
+                    "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+                image: "2.avif",
+                price: 5.99,
+                price: 7.99,
+                quantity: 2,
+            },
+            {
+                id: 4,
+                name: "Enim eiusmod labore",
+                description:
+                    "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+                image: "5.avif",
+                price: 5.99,
+                quantity: 4,
+            },
+        ],
+        total: 68.92,
+        status: "shipped",
+    },
+    {
+        id: 9,
+        customer: "Olivia Davis",
+        products: [
+            {
+                id: 3,
+                name: "Enim eiusmod labore",
+                description:
+                    "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+                image: "4.avif",
+                price: 14.99,
+                quantity: 1,
+            },
+            {
+                id: 0,
+                name: "Enim eiusmod labore",
+                description:
+                    "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+                image: "1.avif",
+                price: 19.99,
+                quantity: 2,
+            },
+        ],
+        total: 54.97,
+        status: "processing",
+    },
+    {
+        id: 10,
+        customer: "Ryan Kim",
+        products: [
+            {
+                id: 1,
+                name: "Enim eiusmod labore",
+                description:
+                    "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+                image: "2.avif",
+                price: 5.99,
+                price: 12.99,
+                quantity: 1,
+            },
+            {
+                id: 3,
+                name: "Enim eiusmod labore",
+                description:
+                    "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+                image: "4.avif",
+                price: 18.99,
+                quantity: 3,
+            },
+            {
+                id: 4,
+                name: "Enim eiusmod labore",
+                description:
+                    "Ipsum do sunt eu cillum eiusmod nostrud anim consequat aliqua. Adipisicing sit ex quis labore veniam ut pariatur pariatur. Occaecat do nulla Lorem laboris quis enim sit anim exercitation.",
+                image: "5.avif",
+                price: 6.99,
+                quantity: 2,
+            },
+        ],
+        total: 95.93,
+        status: "processing",
+    },
+];
