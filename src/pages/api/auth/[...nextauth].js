017d191d36531b5560ed83540e319a4912a74002
KawshikBss <kawshikbss@gmail.com>
2023-04-19 18:19:19 +0600

    modified import for users data

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compare } from "bcryptjs";
-import users from "@/data/users";
+import users from "@/data/usersdata";
 import { searchUser } from "@/services/utils";
 
 const authOptions = {
