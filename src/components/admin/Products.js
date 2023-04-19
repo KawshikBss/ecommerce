31146435f3fbaa70c3122bb8e40848e58081b8fa
KawshikBss <kawshikbss@gmail.com>
2023-04-19 23:14:41 +0600

    added list for products

--- src/components/admin/Products.js
+++ src/components/admin/Products.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import productsdata from "@/data/productsdata";
+import React from "react";
+import ProductListItem from "../ProductListItem";
 
 function Products() {
-  return (
-    <div>Products</div>
-  )
+    return (
+        <div className="w-full h-full bg-white flex flex-col gap-4 items-start shadow-xl p-4 rounded-md overflow-y-scroll">
+            <h3 className="text-[22px] leading-5 font-bold tracking-[0.5px] text-[#212A2F]">
+                Products List
+            </h3>
+            {productsdata.map((product, index) => {
+                return product.admin ? (
+                    ""
+                ) : (
+                    <ProductListItem product={product} key={index} />
+                );
+            })}
+        </div>
+    );
 }
 
-export default Products
+export default Products;
