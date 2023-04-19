0b0c7a146fff0e94c8325d1b0a46757800a400a5
KawshikBss <kawshikbss@gmail.com>
2023-04-19 14:25:34 +0600

    fixed auth routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,11 @@ import {
     AiOutlineSearch,
     AiOutlineUser,
     AiOutlineShoppingCart,
+    AiOutlineLogout,
 } from "react-icons/ai";
-import styles from '@/styles/Header.module.css'
+import styles from "@/styles/Header.module.css";
+import { useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
 
 function Header() {
     const [searchExtended, setSearchExtended] = useState(false);
@@ -13,11 +16,13 @@ function Header() {
         setSearchExtended((curr) => !curr);
     };
 
+    const { data: session, status } = useSession();
+
     return (
         <nav className="flex flex-row justify-between items-center p-[12px]">
             <ul className="flex flex-row justify-between items-center gap-[18px] list-none">
                 <li className="font-bold hover:font-black text-[12px] leading-7 tracking-[1.7px] uppercase text-[#212A2F]">
-                    <Link href={"/"}>Store</Link>
+                    <Link href={"/store"}>Store</Link>
                 </li>
                 <li className="font-bold hover:font-black text-[12px] leading-7 tracking-[1.7px] uppercase text-[#212A2F]">
                     <Link href={"/"}>Trending</Link>
@@ -38,20 +43,39 @@ function Header() {
                 <li className="flex flex-row justify-between items-center font-bold text-[22px] leading-7 tracking-[1.7px] uppercase text-[#212A2F]">
                     <input
                         className={`w-0 border-2 border-black rounded-md mr-5 transition-all duration-300 ease-in-out ${
-                            searchExtended ? styles.search_bar_extended : "border-2 border-transparent"
+                            searchExtended
+                                ? styles.search_bar_extended
+                                : "border-2 border-transparent"
                         }`}
                     />
                     <Link href={"/"}>
                         <AiOutlineSearch onClick={toggleSearchBar} />
                     </Link>
                 </li>
+                {(status === "unauthenticated" ||
+                    (session?.user && session.user.name === "admin")) && (
+                    <li className="font-bold text-[22px] leading-7 tracking-[1.7px] uppercase text-[#212A2F]">
+                        <Link
+                            href={
+                                session?.user && status === "authenticated"
+                                    ? "/admin"
+                                    : "/auth"
+                            }
+                        >
+                            <AiOutlineUser />
+                        </Link>
+                    </li>
+                )}
+                {session?.user && status === "authenticated" && (
+                    <li
+                        className="font-bold text-[22px] leading-7 tracking-[1.7px] uppercase text-[#212A2F] cursor-pointer"
+                        onClick={() => signOut()}
+                    >
+                        <AiOutlineLogout />
+                    </li>
+                )}
                 <li className="font-bold text-[22px] leading-7 tracking-[1.7px] uppercase text-[#212A2F]">
-                    <Link href={"/user"}>
-                        <AiOutlineUser />
-                    </Link>
-                </li>
-                <li className="font-bold text-[22px] leading-7 tracking-[1.7px] uppercase text-[#212A2F]">
-                    <Link href={"/"}>
+                    <Link href={"/cart"}>
                         <AiOutlineShoppingCart />
                     </Link>
                 </li>
