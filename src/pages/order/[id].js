1a1e304cabfc4c4b533bd932ea3c8e3d9ff51af6
KawshikBss <kawshikbss@gmail.com>
2023-04-19 23:49:01 +0600

    added customer and order details

--- src/pages/order/[id].js
+++ src/pages/order/[id].js
@@ -11,23 +11,49 @@ function OrderDetails() {
     const order = id < ordersdata.length ? ordersdata[id] : ordersdata[0];
     return (
         <div className="w-full flex flex-col justify-between py-[27px] px-7">
-            <div className="w-1/3 flex flex-col justify-between items-center gap-6 shadow-xl p-4 rounded-md">
-                <span className="w-full flex flex-row justify-between items-center text-[#212A2F] text-[16px] leading-5 font-bold tracking-[0.5px]">
-                    <span>Sub Total:</span>
-                    <span>${order.total ? order.total : "0"}</span>
-                </span>
-                <span className="w-full flex flex-row justify-between items-center text-[#212A2F] text-[16px] leading-5 font-bold tracking-[0.5px]">
-                    <span>Discount:</span>
-                    <span>$0.0</span>
-                </span>
-                <span className="w-full flex flex-row justify-between items-center text-[#212A2F] text-[16px] leading-5 font-bold tracking-[0.5px]">
-                    <span>Tax:</span>
-                    <span>$0.0</span>
-                </span>
-                <span className="w-full flex flex-row justify-between items-center border-t-2 border-[#212A2F] pt-4 text-[#212A2F] text-[20px] leading-5 font-bold tracking-[0.5px]">
-                    <span>Total:</span>
-                    <span>${order.total ? order.total : "0"}</span>
-                </span>
+            <div className="w-full flex flex-row justify-between items-center">
+                <div className="w-2/5 flex flex-col justify-between items-center gap-6 shadow-xl p-4 rounded-md">
+                    <span className="w-full flex flex-row justify-between items-center text-[#212A2F] text-[16px] leading-5 font-bold tracking-[0.5px]">
+                        <span>Customer:</span>
+                        <span>
+                            {order.customer ? order.customer : "Customer"}
+                        </span>
+                    </span>
+                    <span className="w-full flex flex-row justify-between items-center text-[#212A2F] text-[16px] leading-5 font-bold tracking-[0.5px]">
+                        <span>Phone:</span>
+                        <span>{order.phone ? order.phone : "N/A"}</span>
+                    </span>
+                    <span className="w-full flex flex-row justify-between items-center text-[#212A2F] text-[16px] leading-5 font-bold tracking-[0.5px]">
+                        <span>Shipping Addres:</span>
+                        <span>
+                            {order.shipping_address
+                                ? order.shipping_address
+                                : "N/A"}
+                        </span>
+                    </span>
+                    <span className="w-full flex flex-row justify-between items-center text-[#212A2F] text-[16px] leading-5 font-bold tracking-[0.5px]">
+                        <span>Customer:</span>
+                        <span>
+                            {order.customer ? order.customer : "Customer"}
+                        </span>
+                    </span>
+                    <span className="w-full flex flex-row justify-between items-center text-[#212A2F] text-[16px] leading-5 font-bold tracking-[0.5px]">
+                        <span>Payment:</span>
+                        <span>
+                            {order.payment ? order.payment : "Cash On Delivery"}
+                        </span>
+                    </span>
+                    <span className="w-full flex flex-row justify-between items-center text-[#212A2F] text-[16px] leading-5 font-bold tracking-[0.5px]">
+                        <span>Status:</span>
+                        <span className="uppercase">
+                            {order?.status ? order.status : "processing"}
+                        </span>
+                    </span>
+                    <span className="w-full flex flex-row justify-between items-center border-[#212A2F] pt-4 text-[#212A2F] text-[20px] leading-5 font-bold tracking-[0.5px]">
+                        <span>Bill:</span>
+                        <span>${order.total ? order.total : "0"}</span>
+                    </span>
+                </div>
             </div>
             <div className="w-full flex flex-col justify-between items-start gap-5 shadow-xl p-4 rounded-md">
                 <h3 className="text-[22px] leading-5 font-bold tracking-[0.5px] text-[#212A2F]">
