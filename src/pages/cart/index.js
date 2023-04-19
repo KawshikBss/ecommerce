44aef426ed7f84ae3406c57fd55fd3ad4a7a6646
KawshikBss <kawshikbss@gmail.com>
2023-04-19 15:19:51 +0600

    added cart functionality

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -5,17 +5,34 @@ import products from "@/data/products";
 import React, { useContext } from "react";
 
 function Cart() {
-    const { items } = useContext(CartCtx);
+    const {
+        items,
+        totalUniqueItems,
+        isEmpty,
+        cartTotal,
+        updateItemQuantity,
+        emptyCart,
+    } = useContext(CartCtx);
+    console.log(items);
     return (
         <div className="w-full flex flex-col justify-between py-[27px] px-7">
             <div className="w-full flex flex-row justify-evenly items-start gap-8">
                 <div className="w-full flex flex-col justify-between items-start gap-5 shadow-xl p-4 rounded-md">
                     <h3 className="text-[22px] leading-5 font-bold tracking-[0.5px] text-[#212A2F]">
-                        My Cart (0)
+                        My Cart ({totalUniqueItems ? totalUniqueItems : 0})
                     </h3>
                     {items.length > 0 ? (
                         items.map((item, index) => {
-                            return <CartItem item={item} key={index} />;
+                            return (
+                                <CartItem
+                                    itemId={item.id}
+                                    item={item.item}
+                                    price={item.totalPrice}
+                                    quantity={item.quantity}
+                                    updateItemQuantity={updateItemQuantity}
+                                    key={index}
+                                />
+                            );
                         })
                     ) : (
                         <h3 className="text-[16px] leading-5 font-bold tracking-[0.5px] text-[#212A2F]">
@@ -26,7 +43,7 @@ function Cart() {
                 <div className="w-1/3 flex flex-col justify-between items-center gap-6 shadow-xl p-4 rounded-md">
                     <span className="w-full flex flex-row justify-between items-center text-[#212A2F] text-[16px] leading-5 font-bold tracking-[0.5px]">
                         <span>Sub Total:</span>
-                        <span>$0.0</span>
+                        <span>${cartTotal ? cartTotal : "0"}</span>
                     </span>
                     <span className="w-full flex flex-row justify-between items-center text-[#212A2F] text-[16px] leading-5 font-bold tracking-[0.5px]">
                         <span>Discount:</span>
@@ -38,7 +55,7 @@ function Cart() {
                     </span>
                     <span className="w-full flex flex-row justify-between items-center border-t-2 border-[#212A2F] pt-4 text-[#212A2F] text-[20px] leading-5 font-bold tracking-[0.5px]">
                         <span>Total:</span>
-                        <span>$0.0</span>
+                        <span>${cartTotal ? cartTotal : "0"}</span>
                     </span>
                     <Button label="Checkout" />
                 </div>
